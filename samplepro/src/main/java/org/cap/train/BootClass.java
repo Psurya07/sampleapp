76f5493478a65edd4db3182592d594b5d46f257d
BINNYKARTHIK <karthik.edu.in@gmail.com>
2021-09-20 11:01:32 +0530

    new method added

--- samplepro/src/main/java/org/cap/train/BootClass.java
+++ samplepro/src/main/java/org/cap/train/BootClass.java
@@ -5,7 +5,10 @@ public class BootClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello! from STS!");
-
+		show();
 	}
-
+	
+	public static void show() {
+		System.out.println("Show method here!");
+	}
 }
