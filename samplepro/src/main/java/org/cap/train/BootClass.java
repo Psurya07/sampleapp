400cfc11b216eca16d4ca7b3fd9c4862d9330406
Psurya07 <prgokava@LIN65003985.corp.capgemini.com>
2021-09-20 11:31:13 +0530

    committed first

--- samplepro/src/main/java/org/cap/train/BootClass.java
+++ samplepro/src/main/java/org/cap/train/BootClass.java
@@ -9,12 +9,7 @@ public class BootClass {
 	}
 	
 	public static void show() {
-		int[] arr= {1,2,3};
-		int sum=0;
-		for(int n:arr) {
-			sum+=n;
-			System.out.println(n);
-		}
+		
 		System.out.println("Show method here!");
 	}
 }
