69d090143415bb47bb4eb9e07504fc6d94dedbf9
BINNYKARTHIK <karthik.edu.in@gmail.com>
2021-09-20 11:10:56 +0530

    sum

--- samplepro/src/main/java/org/cap/train/BootClass.java
+++ samplepro/src/main/java/org/cap/train/BootClass.java
@@ -9,6 +9,12 @@ public class BootClass {
 	}
 	
 	public static void show() {
+		int[] arr= {1,2,3};
+		int sum=0;
+		for(int n:arr) {
+			sum+=n;
+			System.out.println(n);
+		}
 		System.out.println("Show method here!");
 	}
 }
